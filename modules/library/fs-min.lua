7968f2a4f589877a77dd7148b9cc529eb92b5be4
Izaya <redtailedairways@gmail.com>
2017-04-23 19:31:13 +1000

    fixed an error in the fs lib

--- modules/library/fs-min.lua
+++ modules/library/fs-min.lua
@@ -2,5 +2,5 @@ fT = {}
 function fres(p)
  local F = (p:match("(%a-):") or p:match("/?(%a-)/"))
  local P = (p:match("%a-:(.+)") or p:match("/?%a-/(.+)"))
- if fT[F] ~= nil then return fT[fid],P else return false end
+ if fT[F] ~= nil then return fT[F],P else return false end
 end

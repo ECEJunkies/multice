a138a02d61a85b8b273e5448983d4c1606e8862f
XeonSquared <redtailedairways@gmail.com>
2017-05-14 22:32:15 +1000

    Reminder not to code before or after midnight

--- modules/library/base64-passthrough.lua
+++ modules/library/base64-passthrough.lua
@@ -1,8 +1,6 @@
-do
 function b64(data)
  return component.invoke(component.list("data")(),"encode64",data)
 end
-
 function ub64(data)
  return component.invoke(component.list("data")(),"decode64",data)
 end
